117b44a65e1374bf5c3398544ba3d2f08ca79b04
Marcus Secato <marcus.secato@symphony.com>
2020-06-03 11:59:10 -0300

    setting dealer name according to personSelector

--- src/main/java/com/symphony/bdk/bot/template/elements/QuoteRegistrationHandler.java
+++ src/main/java/com/symphony/bdk/bot/template/elements/QuoteRegistrationHandler.java
@@ -1,6 +1,7 @@
 package com.symphony.bdk.bot.template.elements;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
@@ -8,7 +9,10 @@ import java.util.regex.Pattern;
 import com.symphony.bdk.bot.sdk.command.model.BotCommand;
 import com.symphony.bdk.bot.sdk.elements.ElementsHandler;
 import com.symphony.bdk.bot.sdk.event.model.SymphonyElementsEvent;
+import com.symphony.bdk.bot.sdk.symphony.UsersClient;
+import com.symphony.bdk.bot.sdk.symphony.exception.SymphonyClientException;
 import com.symphony.bdk.bot.sdk.symphony.model.SymphonyMessage;
+import com.symphony.bdk.bot.sdk.symphony.model.SymphonyUser;
 
 /**
  * Sample code. Implementation of {@link ElementsHandler} which renders a Symphony elements form and
@@ -21,6 +25,12 @@ public class QuoteRegistrationHandler extends ElementsHandler {
   private static final String AMOUNT = "amount";
   private static final String ASSIGNED_TO = "assignedTo";
 
+  private final UsersClient usersClient;
+  
+  public QuoteRegistrationHandler(UsersClient usersClient) {
+    this.usersClient = usersClient;
+  }
+  
   /**
    * Used by CommandFilter to filter Symphony chat messages
    */
@@ -52,15 +62,19 @@ public class QuoteRegistrationHandler extends ElementsHandler {
   @Override
   public void handleAction(SymphonyElementsEvent event, SymphonyMessage elementsResponse) {
     Map<String, Object> formValues = event.getFormValues();
-
     Map<String, Object> data = new HashMap<String, Object>();
     data.put(FROM_CURRENCY, formValues.get(FROM_CURRENCY).toString().toUpperCase());
     data.put(TO_CURRENCY, formValues.get(TO_CURRENCY).toString().toUpperCase());
     data.put(AMOUNT, formValues.get(AMOUNT));
-    data.put(ASSIGNED_TO, event.getUser().getDisplayName());
-
-    elementsResponse.setTemplateMessage(
-        "Quote FX {{fromCurrency}}-{{toCurrency}} {{amount}} sent to dealer {{assignedTo}}", data);
+    try {
+      List<Long> personSelector = (List<Long>)formValues.get(ASSIGNED_TO);
+      SymphonyUser dealer = usersClient.getUserFromId(personSelector.get(0), true);
+      data.put(ASSIGNED_TO, dealer.getDisplayName());
+      elementsResponse.setTemplateMessage(
+          "Quote FX {{fromCurrency}}-{{toCurrency}} {{amount}} sent to dealer {{assignedTo}}", data);
+    } catch (SymphonyClientException sce) {
+      // Handle failure retrieving dealer details
+    }
   }
 
 }
